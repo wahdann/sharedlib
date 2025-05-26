dd557e8778f0371c8ec273fadb8f27116c003431
Ahmed Wahdan <xwahdanx@gmail.com>
2025-05-27 00:31:35 +0300

    nn

--- src/org/iti/docker.groovy
+++ src/org/iti/docker.groovy
@@ -8,7 +8,8 @@ def build(IMAGE_NAME, IMAGE_TAG){
     sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
 }
 
-def push(USER_NAME,IMAGE_NAME, IMAGE_TAG){
-    sh "docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${USER_NAME}/${IMAGE_NAME}:${IMAGE_TAG}"
-    sh "docker push ${USER_NAME}/${IMAGE_NAME}:${IMAGE_TAG}"
+def push(IMAGE_NAME,USERNAME, IMAGE_TAG){
+    sh "docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${USERNAME}/${IMAGE_NAME}:${IMAGE_TAG}"
+
+    sh "docker push ${USERNAME}/${IMAGE_NAME}:${IMAGE_TAG}"
 }
